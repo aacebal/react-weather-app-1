1e2bbb74718f0e5eccc20e773b60b4061c3a90d4
Adel Acebal <adelacebal@yahoo.com>
2017-08-18 20:23:21 -0400

    adding elements to the DOM

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 import './styles.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('Hello, world!');
+const root = document.getElementById('app-container');
+
+ReactDOM.render(<p>Hello, world!</p>, root);
